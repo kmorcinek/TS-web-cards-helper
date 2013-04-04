bffce3c41983198b73f7b014694fc51aa3baa941
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2013-04-04 11:46:29 +0200

    Parser inside WebProject.

---
+++ src/KrzysztofMorcinek.TSCardsHelper.Web/CardParser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace KrzysztofMorcinek.TSCardsHelper.Web
+{
+    public class CardParser
+    {
+        public static IEnumerable<string> ParseCardsPage(string path)
+        {
+            var doc = new XmlDocument();
+            doc.Load(path);
+
+            var tbody = doc.ChildNodes[0];
+
+            var names = new List<string>();
+
+            foreach (XmlNode row in tbody)
+            {
+                var secondTd = row.ChildNodes[1];
+                names.Add(secondTd.InnerText);
+            }
+
+            return names;
+        }
+    }
+}
--- src/KrzysztofMorcinek.TSCardsHelper.Web/index.aspx.cs
+++ src/KrzysztofMorcinek.TSCardsHelper.Web/index.aspx.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Dynamic;
 using KrzysztofMorcinek.TSCardsHelper.Web.Helpers;
 
 namespace KrzysztofMorcinek.TSCardsHelper.Web
@@ -23,14 +19,15 @@ namespace KrzysztofMorcinek.TSCardsHelper.Web
         {
             var cards = new List<Card>();
 
-            var names = File.ReadAllLines(@"D:\NieWirtualki\Work\GitHub\TS-web-cards-helper\src\KrzysztofMorcinek.TSCardsHelper.Web\lateWarCards.txt");
-
-            foreach (var name in names)
+            foreach (var name in CardParser.ParseCardsPage(@"D:\NieWirtualki\Work\GitHub\TS-web-cards-helper\src\KrzysztofMorcinek.TSCardsHelper.Web\DataFiles\EarlyWar.xml"))
             {
                 cards.Add(new Card { Name = name, CanRemove = name.EndsWith("*") });
             }
 
-            return JsonNetSerializer.SerializeToString(cards);
+            dynamic x = new ExpandoObject();
+            x.EarlyWar = cards.ToArray();
+
+            return JsonNetSerializer.SerializeToString(x);
         }
     }
 }
