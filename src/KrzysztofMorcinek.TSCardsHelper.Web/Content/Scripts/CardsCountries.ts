68bb97ecf9e16f5a2d44635a596895b13ae93079
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2013-04-08 16:18:24 +0200

    UnderscoreJS added.

--- src/KrzysztofMorcinek.TSCardsHelper.Web/Content/Scripts/CardsCountries.ts
+++ src/KrzysztofMorcinek.TSCardsHelper.Web/Content/Scripts/CardsCountries.ts
@@ -1,10 +1,15 @@
 import cardModule = module("./Card");
 import countryModule = module("./Country");
 
+declare var _: any;
+
 export class CardsCountries {
     constructor(public cards: cardModule.Card[], public countries: countryModule.Country) { }
 
     getCards(countryId: number) {
-        return [this.cards[0]];
+        var returningCards = _.filter(this.cards, function (card) {
+            return _.contains(this.countries[countryId].cardIds, card.id);
+        });
+        return returningCards;
     }
 }
--- src/KrzysztofMorcinek.TSCardsHelper.Web/Content/Scripts/map.js
+++ src/KrzysztofMorcinek.TSCardsHelper.Web/Content/Scripts/map.js
@@ -1,5 +1,11 @@
 ﻿function showForCountry(countryId) {
-    var d = new Cards.Card(1, "dd");
+    var cards = [new Card(1, "Defectors"), new Card(2, "Europe")];
+    var idx = [2];
+    var returningCards = _.filter(cards, function(card) {
+        return _.contains(idx, card.id);
+    });
+    
+    var d = new Card(1, "dd");
     alert(d.id);
     document.myform.stage.value = countryId;
 }
