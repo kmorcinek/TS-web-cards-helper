e6dd44861a1a29dae88637d4f43d948177710fe5
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2013-04-20 22:52:39 +0200

    earlyWar cards into map.

--- src/KrzysztofMorcinek.TSCardsHelper.Web/index.aspx.cs
+++ src/KrzysztofMorcinek.TSCardsHelper.Web/index.aspx.cs
@@ -4,6 +4,8 @@ using System.Dynamic;
 using System.Linq;
 using KrzysztofMorcinek.TSCardsHelper.Web.Helpers;
 using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace KrzysztofMorcinek.TSCardsHelper.Web
 {
@@ -19,20 +21,20 @@ namespace KrzysztofMorcinek.TSCardsHelper.Web
 
         protected string GetCards()
         {
-            dynamic x = new ExpandoObject();
+            dynamic cards = new ExpandoObject();
 
             // TODO annoying right path
             var basePath = @"D:\Work\Git\TS-web-cards-helper\src";
-            x.EarlyWar = CardParser.ParseCardsPage(Path.Combine(basePath, @"KrzysztofMorcinek.TSCardsHelper.Web\DataFiles\EarlyWar.xml"))
+            cards.EarlyWar = CardParser.ParseCardsPage(Path.Combine(basePath, @"KrzysztofMorcinek.TSCardsHelper.Web\DataFiles\EarlyWar.xml")).Select(card => new {Id = card.Id, Name = card.Name})
                 .ToArray();
 
-            x.MidWar = CardParser.ParseCardsPage(Path.Combine(basePath, @"KrzysztofMorcinek.TSCardsHelper.Web\DataFiles\MidWarToParse.xml"))
-                .ToArray();
+            //x.MidWar = CardParser.ParseCardsPage(Path.Combine(basePath, @"KrzysztofMorcinek.TSCardsHelper.Web\DataFiles\MidWarToParse.xml"))
+            //    .ToArray();
 
-            x.LateWar = CardParser.ParseCardsPage(Path.Combine(basePath, @"KrzysztofMorcinek.TSCardsHelper.Web\DataFiles\LateWarToParsexml.xml"))
-                .ToArray();
-
-            return JsonNetSerializer.SerializeToString(x);
+            //x.LateWar = CardParser.ParseCardsPage(Path.Combine(basePath, @"KrzysztofMorcinek.TSCardsHelper.Web\DataFiles\LateWarToParsexml.xml"))
+            //    .ToArray();
+            var jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+            return JsonConvert.SerializeObject(cards, Formatting.Indented, jsonSerializerSettings);
         }
     }
 }
