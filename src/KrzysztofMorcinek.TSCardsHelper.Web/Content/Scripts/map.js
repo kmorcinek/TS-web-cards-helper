d9fa152e2d6e8182ae07e94b9005bc6d17b0bd09
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2013-04-20 21:18:59 +0200

    Europe regions only

--- src/KrzysztofMorcinek.TSCardsHelper.Web/Content/Scripts/map.js
+++ src/KrzysztofMorcinek.TSCardsHelper.Web/Content/Scripts/map.js
@@ -7,35 +7,37 @@
     ];
 
 var Region = (function () {
-    function Region(id, name) {
+    function Region(id, name, firstCountryId, lastCountryId) {
         this.id = id;
         this.name = name;
+        this.firstCountryId = firstCountryId;
+        this.lastCountryId = lastCountryId;
     }
     return Region;
 })();
 
 var regions = [
-    new Region(1, "Europe"),
-    new Region(11, "West Europe"),
-    new Region(12, "East Europe"),
-    new Region(2, "Middle East"),
-    new Region(3, "Asia"),
-    new Region(31, "SE Asia"),
-    new Region(4, "Africa"),
-    new Region(5, "Central America"),
-    new Region(6, "South America"),
+    new Region(1, "Europe", 0, 8),
+    new Region(11, "West Europe", 0, 0),
+    new Region(12, "East Europe", 0, 0),
+//    new Region(2, "Middle East"),
+//    new Region(3, "Asia"),
+//    new Region(31, "SE Asia"),
+//    new Region(4, "Africa"),
+//    new Region(5, "Central America"),
+//    new Region(6, "South America"),
     ];
 
 var countries = [
-    new Country(1, "Canada", [2], 230, 195),
-    new Country(2, "UK", [2, 28, 105], 460, 147),
-    new Country(3, "Spain/Portugal", [], 450, 295),
-    new Country(4, "France", [17, 28], 494, 239),
-    new Country(5, "Benelux", [105], 506, 190),
-    new Country(6, "Norway", [], 525, 68),
-    new Country(7, "Denmark", [], 538, 110),
-    new Country(8, "W.Germany", [10], 566, 190),
-    new Country(9, "Italy", [], 576, 273),
+    new Country(0, "Canada", [2], 230, 195),
+    new Country(1, "UK", [2, 28, 105], 460, 147),
+    new Country(2, "Spain/Portugal", [], 450, 295),
+    new Country(3, "France", [17, 28], 494, 239),
+    new Country(4, "Benelux", [105], 506, 190),
+    new Country(5, "Norway", [], 525, 68),
+    new Country(6, "Denmark", [], 538, 110),
+    new Country(7, "W.Germany", [10], 566, 190),
+    new Country(8, "Italy", [], 576, 273),
 ];
 
 var cc = new CardsCountries(cards, countries, countries);
