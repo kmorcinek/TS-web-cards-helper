194b6d15c69561b3763c016dea3eaf22c0d78ee4
Krzysztof Morcinek <krzysztof.morcinek@gmail.cm>
2013-04-28 22:43:41 +0200

    refactoring

--- src/KrzysztofMorcinek.TSCardsHelper.Web/CardParser.cs
+++ src/KrzysztofMorcinek.TSCardsHelper.Web/CardParser.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Xml;
 
 namespace KrzysztofMorcinek.TSCardsHelper.Web
@@ -12,22 +13,17 @@ namespace KrzysztofMorcinek.TSCardsHelper.Web
 
             var tbody = doc.ChildNodes[0];
 
-            var cards = new List<Card>();
-
-            foreach (XmlNode row in tbody)
-            {
-                var secondTd = row.ChildNodes[1];
-
-                cards.Add(new Card { 
-                    Id = int.Parse(row.ChildNodes[0].InnerText), 
-                    Name = secondTd.InnerText, 
-                    Href = row.ChildNodes[1].ChildNodes[0].Attributes["href"].InnerText,
-                    Ops = int.Parse(row.ChildNodes[2].InnerText), 
-                    Description = row.ChildNodes[3].InnerText, 
-                    CanRemove = secondTd.InnerText.EndsWith("*") });
-            }
-
-            return cards;
+            return (from XmlNode row in tbody
+                    let secondTd = row.ChildNodes[1]
+                    select new Card
+                        {
+                            Id = int.Parse(row.ChildNodes[0].InnerText), 
+                            Name = secondTd.InnerText, 
+                            Href = row.ChildNodes[1].ChildNodes[0].Attributes["href"].InnerText, 
+                            Ops = int.Parse(row.ChildNodes[2].InnerText), 
+                            Description = row.ChildNodes[3].InnerText, 
+                            CanRemove = secondTd.InnerText.EndsWith("*")
+                        }).ToList();
         }
     }
 }
