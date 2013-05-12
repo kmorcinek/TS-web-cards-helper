b9f200ec13cce2924c4c8d53bae8a0150a23f68f
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2013-05-12 18:45:01 +0200

    Code indenting.

--- src/KrzysztofMorcinek.TSCardsHelper.Web/Content/Scripts/map.js
+++ src/KrzysztofMorcinek.TSCardsHelper.Web/Content/Scripts/map.js
@@ -13,14 +13,14 @@ var cardsForCountries = [
 { "id": 3, "countryIds": [], "regionIds": [3] }, // Middle East Scoring
 { "id": 4, "countryIds": [], "regionIds": [] }, // Duck and Cover
 { "id": 5, "countryIds": [], "regionIds": [] }, // Five Year Plan
-{ "id": 6, "countryIds": [31,32,36], "regionIds": [4] }, // The China Card
+{ "id": 6, "countryIds": [31, 32, 36], "regionIds": [4] }, // The China Card
 { "id": 7, "countryIds": [], "regionIds": [1] }, // Socialist Governments
 { "id": 8, "countryIds": [71], "regionIds": [] }, // Fidel*
 { "id": 9, "countryIds": [37], "regionIds": [5] }, // Vietnam Revolts*
 { "id": 10, "countryIds": [7], "regionIds": [] }, // Blockade*
-{ "id": 11, "countryIds": [42,43,44,45], "regionIds": [] }, // Korean War*
+{ "id": 11, "countryIds": [42, 43, 44, 45], "regionIds": [] }, // Korean War*
 { "id": 12, "countryIds": [20], "regionIds": [] }, // Romanian Abdication*
-{ "id": 13, "countryIds": [21,22,23,27,28], "regionIds": [] }, // Arab-Israeli War
+{ "id": 13, "countryIds": [21, 22, 23, 27, 28], "regionIds": [] }, // Arab-Israeli War
 { "id": 14, "countryIds": [], "regionIds": [2] }, // Comecon*
 { "id": 15, "countryIds": [27], "regionIds": [] }, // Nasser*
 { "id": 16, "countryIds": [], "regionIds": [2] }, // Warsaw Pact Formed*
@@ -31,66 +31,66 @@ var cardsForCountries = [
 { "id": 21, "countryIds": [], "regionIds": [0] }, // NATO*
 { "id": 22, "countryIds": [15, 20, 19, 18, 16], "regionIds": [] }, // Independent Reds*
 { "id": 23, "countryIds": [], "regionIds": [1] }, // Marshall Plan*
-{ "id": 24, "countryIds": [25,31,32,33,34], "regionIds": [] }, // Indo-Pakistani War
+{ "id": 24, "countryIds": [25, 31, 32, 33, 34], "regionIds": [] }, // Indo-Pakistani War
 { "id": 25, "countryIds": [], "regionIds": [] }, // Containment*
 { "id": 26, "countryIds": [], "regionIds": [] }, // CIA Created*
 { "id": 27, "countryIds": [42], "regionIds": [] }, // US/Japan Mutual Defense Pact*
-{ "id": 28, "countryIds": [1,3,23], "regionIds": [] }, // Suez Crisis*
+{ "id": 28, "countryIds": [1, 3, 23], "regionIds": [] }, // Suez Crisis*
 { "id": 29, "countryIds": [], "regionIds": [2] }, // East European Unrest
-{ "id": 30, "countryIds": [], "regionIds": [5,6] }, // Decolonization
+{ "id": 30, "countryIds": [], "regionIds": [5, 6] }, // Decolonization
 { "id": 31, "countryIds": [], "regionIds": [] }, // Red Scare/Purge
 { "id": 32, "countryIds": [], "regionIds": [] }, // UN Intervention
-{ "id": 33, "countryIds": [], "regionIds": [0,3,4,6,7,8] }, // De-Stalinization*
+{ "id": 33, "countryIds": [], "regionIds": [0, 3, 4, 6, 7, 8] }, // De-Stalinization*
 { "id": 34, "countryIds": [], "regionIds": [] }, // Nuclear Test Ban
 { "id": 35, "countryIds": [43], "regionIds": [] }, // Formosan Resolution*
 { "id": 36, "countryIds": [], "regionIds": [] },// Brush War 
 { "id": 37, "countryIds": [], "regionIds": [7] },// Central America Scoring 
 { "id": 38, "countryIds": [], "regionIds": [5] },// Southeast Asia Scoring* 
 { "id": 39, "countryIds": [], "regionIds": [] },// Arms Race 
-{ "id": 40, "countryIds": [7,12,71], "regionIds": [] },// Cuban Missile Crisis* 
+{ "id": 40, "countryIds": [7, 12, 71], "regionIds": [] },// Cuban Missile Crisis* 
 { "id": 41, "countryIds": [], "regionIds": [] },// Nuclear Subs* 
 { "id": 42, "countryIds": [0], "regionIds": [] },// Quagmire* 
 { "id": 43, "countryIds": [], "regionIds": [] },// SALT Negotiations* 
 { "id": 44, "countryIds": [], "regionIds": [] },// Bear Trap* 
 { "id": 45, "countryIds": [], "regionIds": [] },// Summit 
 { "id": 46, "countryIds": [], "regionIds": [] },// How I Learned to Stop Worrying* 
-{ "id": 47, "countryIds": [], "regionIds": [7,8] },// Junta 
+{ "id": 47, "countryIds": [], "regionIds": [7, 8] },// Junta 
 { "id": 48, "countryIds": [], "regionIds": [] },// Kitchen Debates* 
 { "id": 49, "countryIds": [], "regionIds": [] },// Missile Envy 
 { "id": 50, "countryIds": [], "regionIds": [] },// “We Will Bury You”* 
 { "id": 51, "countryIds": [], "regionIds": [] },// Brezhnev Doctrine* 
-{ "id": 52, "countryIds": [55,59], "regionIds": [] },// Portuguese Empire Crumbles* 
-{ "id": 53, "countryIds": [55,56,57], "regionIds": [] },// South African Unrest 
+{ "id": 52, "countryIds": [55, 59], "regionIds": [] },// Portuguese Empire Crumbles* 
+{ "id": 53, "countryIds": [55, 56, 57], "regionIds": [] },// South African Unrest 
 { "id": 54, "countryIds": [80], "regionIds": [] },// Allende* 
 { "id": 55, "countryIds": [7], "regionIds": [] },// Willy Brandt* 
-{ "id": 56, "countryIds": [22,24,25,26,27,28,30,63], "regionIds": [] },// Muslim Revolution 
+{ "id": 56, "countryIds": [22, 24, 25, 26, 27, 28, 30, 63], "regionIds": [] },// Muslim Revolution 
 { "id": 57, "countryIds": [], "regionIds": [] },// ABM Treaty 
 { "id": 58, "countryIds": [], "regionIds": [4] },// Cultural Revolution* 
 { "id": 59, "countryIds": [], "regionIds": [] },// Flower Power* 
 { "id": 60, "countryIds": [], "regionIds": [] },// U2 Incident* 
-{ "id": 61, "countryIds": [24,25,26,27,29,30,75], "regionIds": [] },// OPEC 
+{ "id": 61, "countryIds": [24, 25, 26, 27, 29, 30, 75], "regionIds": [] },// OPEC 
 { "id": 62, "countryIds": [], "regionIds": [] },// “Lone Gunman”* 
-{ "id": 63, "countryIds": [], "regionIds": [5,6] },// Colonial Rear Guards 
+{ "id": 63, "countryIds": [], "regionIds": [5, 6] },// Colonial Rear Guards 
 { "id": 64, "countryIds": [68, 69, 75], "regionIds": [] },// Panama Canal Returned* 
-{ "id": 65, "countryIds": [23,27,28], "regionIds": [] },// Camp David Accords* 
-{ "id": 66, "countryIds": [], "regionIds": [0,3,4,6,7,8] },// Puppet Governments* 
+{ "id": 65, "countryIds": [23, 27, 28], "regionIds": [] },// Camp David Accords* 
+{ "id": 66, "countryIds": [], "regionIds": [0, 3, 4, 6, 7, 8] },// Puppet Governments* 
 { "id": 67, "countryIds": [], "regionIds": [] },// Grain Sales to Soviets 
 { "id": 68, "countryIds": [17], "regionIds": [] },// John Paul II Elected Pope* 
 { "id": 69, "countryIds": [], "regionIds": [7, 8] },// Latin American Death Squads 
 { "id": 70, "countryIds": [], "regionIds": [7, 8] },// OAS Founded* 
 { "id": 71, "countryIds": [], "regionIds": [4] },// Nixon Plays the China Card* 
 { "id": 72, "countryIds": [27], "regionIds": [] },// Sadat Expels Soviets* 
-{ "id": 73, "countryIds": [], "regionIds": [3,4] },// Shuttle Diplomacy 
-{ "id": 74, "countryIds": [], "regionIds": [3,4,6,7,8] },// The Voice of America 
+{ "id": 73, "countryIds": [], "regionIds": [3, 4] },// Shuttle Diplomacy 
+{ "id": 74, "countryIds": [], "regionIds": [3, 4, 6, 7, 8] },// The Voice of America 
 { "id": 75, "countryIds": [], "regionIds": [7] },// Liberation Theology 
 { "id": 76, "countryIds": [], "regionIds": [4] },// Ussuri River Skirmish* 
 { "id": 77, "countryIds": [], "regionIds": [] },// “Ask Not What Your Country…”* 
-{ "id": 78, "countryIds": [], "regionIds": [7,8] },// Alliance for Progress* 
+{ "id": 78, "countryIds": [], "regionIds": [7, 8] },// Alliance for Progress* 
 { "id": 79, "countryIds": [], "regionIds": [6] },// Africa Scoring 
 { "id": 80, "countryIds": [], "regionIds": [] },// “One Small Step…” 
 { "id": 81, "countryIds": [], "regionIds": [8] },// South America Scoring 
 { "id": 82, "countryIds": [25], "regionIds": [] },// Iranian Hostage Crisis*
-{ "id": 83, "countryIds": [1,79], "regionIds": [] },// The Iron Lady*
+{ "id": 83, "countryIds": [1, 79], "regionIds": [] },// The Iron Lady*
 { "id": 84, "countryIds": [26], "regionIds": [] },// Reagan Bombs Libya*
 { "id": 85, "countryIds": [], "regionIds": [] },// Star Wars*
 { "id": 86, "countryIds": [], "regionIds": [] },// North Sea Oil*
@@ -98,10 +98,10 @@ var cardsForCountries = [
 { "id": 88, "countryIds": [21], "regionIds": [3] },// Marine Barracks Bombing*
 { "id": 89, "countryIds": [44], "regionIds": [] },// Soviets Shoot Down KAL-007*
 { "id": 90, "countryIds": [], "regionIds": [0] },// Glasnost*
-{ "id": 91, "countryIds": [67,68,70,71,72], "regionIds": [] },// Ortega Elected in Nicaragua*
+{ "id": 91, "countryIds": [67, 68, 70, 71, 72], "regionIds": [] },// Ortega Elected in Nicaragua*
 { "id": 92, "countryIds": [], "regionIds": [] },// Terrorism
 { "id": 93, "countryIds": [], "regionIds": [] },// Iran-Contra Scandal*
-{ "id": 94, "countryIds": [], "regionIds": [0,3,4,6,7,8] },// Chernobyl*
+{ "id": 94, "countryIds": [], "regionIds": [0, 3, 4, 6, 7, 8] },// Chernobyl*
 { "id": 95, "countryIds": [], "regionIds": [8] },// Latin American Debt Crisis
 { "id": 96, "countryIds": [9], "regionIds": [0] },// Tear Down this Wall*
 { "id": 97, "countryIds": [], "regionIds": [] },// “An Evil Empire”*
@@ -109,12 +109,12 @@ var cardsForCountries = [
 { "id": 99, "countryIds": [], "regionIds": [1] },// Pershing II Deployed*
 { "id": 100, "countryIds": [], "regionIds": [] },// Wargames*
 { "id": 101, "countryIds": [17], "regionIds": [] },// Solidarity*
-{ "id": 102, "countryIds": [24,25,28,29,30,31,32], "regionIds": [] },// Iran-Iraq War*
+{ "id": 102, "countryIds": [24, 25, 28, 29, 30, 31, 32], "regionIds": [] },// Iran-Iraq War*
 { "id": 103, "countryIds": [], "regionIds": [] }, // Defectors
 { "id": 104, "countryIds": [], "regionIds": [] }, // The Cambridge Five
 { "id": 105, "countryIds": [1], "regionIds": [1] }, // Special Relationship
 { "id": 106, "countryIds": [0], "regionIds": [] }, // NORAD*
-{ "id": 107, "countryIds": [], "regionIds": [6,7,8] },// Che 
+{ "id": 107, "countryIds": [], "regionIds": [6, 7, 8] },// Che 
 { "id": 108, "countryIds": [], "regionIds": [3] },// Our Man in Tehran*
 { "id": 109, "countryIds": [], "regionIds": [] },// Yuri and Samantha*
 { "id": 110, "countryIds": [30], "regionIds": [3] },// AWACS Sale to Saudis*
@@ -138,7 +138,7 @@ var regions = [
     new Region("Africa", 46, 63),
     new Region("Central America", 64, 73),
     new Region("South America", 74, 83),
-    ];
+];
 
 var countries = [
     new Country(0, "Canada", 230, 195),
